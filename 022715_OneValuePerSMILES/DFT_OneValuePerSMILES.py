dbb63128647cc4995b197a49596b08f4dcce43c8
Ching-Yen <achiiin63@gmail.com>
2015-02-27 16:43:14 -0500

    Initial setup_1

---
+++ 022715_OneValuePerSMILES/DFT_OneValuePerSMILES.py
@@ -0,0 +1,24 @@
+"""
+Created on Fri Feb 27 15:15:28 2015
+@author: Ching-Yen
+"""
+"""
+After the ananlyse of Coefficient of Variance(CV) for each SMILES group of 
+each property, I found that the variation of values in each SMILES group of HOMO is 
+small. Thus, I would like to try to use the median value of each SMILES group to 
+represent the entire group.
+
+Basically, I will just write a few new lines to import the median values and
+the remaining work I will leave for the program I have done before to finish them.
+ 
+
+The inputs:
+DFT_(property)_(index)_(flavorName).csv 
+SMILES,amount of this SMILES, max, min, median, mean, MAD, SD, CV
+
+
+The ouputs:
+
+
+
+"""
