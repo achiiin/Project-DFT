8208722301c0c77f0af148fb04cbacb8aad87634
Ching-Yen <achiiin63@gmail.com>
2015-02-28 17:49:59 -0500

    Code: Test Si Se upper lower case

---
+++ 022815_testSiSe UpperLowerCase/DFT_SiSeTestCase.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Feb 28 17:44:07 2015
+
+@author: Ching-Yen
+"""
+
+num_Si_lowecase=0
+num_se_uppercase = 0
+
+f = open('uni_SMILES.txt')
+for smiles in f:
+    num_Si_lowecase = num_Si_lowecase + smiles.count('si')
+    num_se_uppercase = num_se_uppercase + smiles.count('Se')
+f.close()
+print "si:  ", num_Si_lowecase
+print "Se:  ", num_se_uppercase
