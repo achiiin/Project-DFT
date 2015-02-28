60573b859518a1cf2333d7b41fdbb9c42448cf53
Ching-Yen <achiiin63@gmail.com>
2015-02-28 17:58:20 -0500

    Code: correct error

--- 022815_testSiSe UpperLowerCase/DFT_SiSeTestCase.py
+++ 022815_testSiSe UpperLowerCase/DFT_SiSeTestCase.py
@@ -10,8 +10,8 @@ num_se_uppercase = 0
 
 f = open('uni_SMILES.txt')
 for smiles in f:
-    num_Si_lowecase = num_Si_lowecase + smiles.count('si')
-    num_se_uppercase = num_se_uppercase + smiles.count('Se')
+    num_Si_lowecase = num_Si_lowecase + str(smiles).count('si')
+    num_se_uppercase = num_se_uppercase + str(smiles).count('Se')
 f.close()
 print "si:  ", num_Si_lowecase
 print "Se:  ", num_se_uppercase
