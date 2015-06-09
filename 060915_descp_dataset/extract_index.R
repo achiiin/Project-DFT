0e7930bf7f9af8224b316241c273f94c20a4fad7
Ching-Yen <achiiin63@gmail.com>
2015-06-09 04:59:03 -0400

    create extract_index.R

---
+++ 060915_descp_dataset/extract_index.R
@@ -0,0 +1,13 @@
+df_1 <- read.csv('test_1.csv')
+
+df_2 <- read.table('uni_SMILES_300.txt')
+colnames(df_2) <- "SMILES"
+
+df_2$No. <- 1:nrow(df_2)
+
+df_3 <- merge(df_1,df_2,by="SMILES")
+
+df_4 <- df_3[ , which(names(df_3) %in% c("No.","F18"))]
+df_4 <- df_4[c("No.","F18")]
+df_4 <- df_4[order(df_4[,"No."]),]
+write.csv(df_4,file="F18_ID_class_v1.0.csv",row.names=FALSE,quote=FALSE)
