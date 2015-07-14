c90d6f45353307dab71b38386053f1b64fe0f644
Ching-Yen <achiiin63@gmail.com>
2015-07-13 23:03:12 -0400

    update

--- 060315_ML_v2.0/DFT_predict_v2.3.2.R
+++ 060315_ML_v2.0/DFT_predict_v2.3.2.R
@@ -8,8 +8,8 @@
 #        only consider class a,d,e,f, regroup a as "0", and d,e,f as "1"
 # v2.3. : add the measurements such as the area under the ROC curve, the sensitivity,
 #         and spcificity
-
-
+#      v2.3.2: using sampling methods 1. up-sampling 2. downsampling 3.SMOTE
+#              (60% training & 20% testing & 20% validation)
 select_flavors <- "F18"
 df_bb <- read.csv('BB_test_3.csv')
 df_outlier <- read.csv('BB_test_3_2.csv')
@@ -56,23 +56,9 @@ bad <- is.na(df_bb$Class)
 df_bb <- df_bb[!bad,]
 print(dim(df_bb))
 print(summary(df_bb$Class))
-inTrain = createDataPartition(df_bb$Class, p = 3/5)[[1]]
-training = df_bb[ inTrain,-c(1)]
-testing_1 = df_bb[-inTrain,-c(1)]
-print(names(training))
-print(names(testing_1))
-set.seed(1990)
-inTest = createDataPartition(testing_1$Class, p = 1/2)[[1]]
-testing = testing_1[inTest,]
-validation = testing_1[-inTest,]
 
-#### create training(80%) & testing(20%) ####
-# set.seed(1990)
-# inTrain_80 = createDataPartition(df_bb$Class, p = 4/5)[[1]]
-# training_80 = df_bb[ inTrain_80,-c(1)]
-# testing_80 = df_bb[-inTrain_80,-c(1)]
-# print(names(training_80))
-# print(summary(training_80$Class))
+
+
 
 #### regroup a as "0", and d,e,f as "1" ####
 set.seed(1990)
@@ -85,39 +71,57 @@ df_bb_2$Class[df_bb_2$Class %in% c("d","e","f")] <- "out"
 df_bb_2$Class <- as.factor(df_bb_2$Class)
 print(summary(df_bb_2$Class))
 
-inTrain_80 = createDataPartition(df_bb_2$Class, p = 4/5)[[1]]
-training_80 = df_bb_2[ inTrain_80,-c(1)]
-testing_80 = df_bb_2[-inTrain_80,-c(1)]
-print(names(training_80))
-print(summary(training_80$Class))
+### for 60% training & 20% testing & 20% validation###
+inTrain_60 = createDataPartition(df_bb_2$Class, p = 3/5)[[1]]
+training = df_bb_2[ inTrain_60,-c(1)]
+testing_40 = df_bb_2[-inTrain_60,-c(1)]
+print(names(training))
+print(dim(training))
+print(summary(training$Class))
+set.seed(1990)
+inTest = createDataPartition(testing_40$Class, p = 1/2)[[1]]
+testing = testing_40[inTest,]
+validation = testing_40[-inTest,]
+print(names(testing))
+print(dim(testing))
+print(summary(testing$Class))
+
+######################### Sampling Methods ############################
+#### Up-Sampling####
+set.seed(63)
+#### Down-Sampling ####
+set.seed(63)
+#### synthetic minority over-sampling technique (SMOTE) ####
+set.seed(63)
+
 
 
 #### Neural Networks ####
 # set.seed(1990)
-# Fit_nnet <- train(Class~., method="nnet", data = training_80,trace = FALSE)
+# Fit_nnet <- train(Class~., method="nnet", data = training,trace = FALSE)
 # 
 # print(Fit_nnet);print(Fit_nnet$finalModel)
 # 
-# pd_train_nnet <- predict(Fit_nnet,training_80)
-# print(confusionMatrix(data = pd_train_nnet,reference = training_80$Class)$table)
-# print(confusionMatrix(data = pd_train_nnet,reference = training_80$Class)$overall[1])
+# pd_train_nnet <- predict(Fit_nnet,training)
+# print(confusionMatrix(data = pd_train_nnet,reference = training$Class)$table)
+# print(confusionMatrix(data = pd_train_nnet,reference = training$Class)$overall[1])
 # 
-# pd_test_nnet <- predict(Fit_nnet,testing_80)
+# pd_test_nnet <- predict(Fit_nnet,testing)
 # 
-# print(confusionMatrix(data = pd_test_nnet,reference = testing_80$Class)$table)
-# print(confusionMatrix(data = pd_test_nnet,reference = testing_80$Class)$overall[1])
+# print(confusionMatrix(data = pd_test_nnet,reference = testing$Class)$table)
+# print(confusionMatrix(data = pd_test_nnet,reference = testing$Class)$overall[1])
 ####Random Forest####
-set.seed(1990)
-fitControl <- trainControl(method = "none",classProbs = TRUE,summaryFunction = twoClassSummary)
-tgrid <- expand.grid(mtry=c(6)) 
-Fit_rf <- train(Class~., trControl = fitControl, tuneGrid=tgrid,data=training_80,
-                method = 'rf',ntree = 500, metric="ROC")
-
-print(Fit_rf);print(Fit_rf$finalModel)
-
-pd_test_rf <- predict(Fit_rf,testing_80)
-print(confusionMatrix(data = pd_test_rf,reference = testing_80$Class)$table)
-print(confusionMatrix(data = pd_test_rf,reference = testing_80$Class)$overall[1])
+# set.seed(1990)
+# fitControl <- trainControl(method = "none",classProbs = TRUE,summaryFunction = twoClassSummary)
+# tgrid <- expand.grid(mtry=c(6)) 
+# Fit_rf <- train(Class~., trControl = fitControl, tuneGrid=tgrid,data=training,
+#                 method = 'rf',ntree = 500, metric="ROC")
+# 
+# print(Fit_rf);print(Fit_rf$finalModel)
+# 
+# pd_test_rf <- predict(Fit_rf,testing)
+# print(confusionMatrix(data = pd_test_rf,reference = testing$Class)$table)
+# print(confusionMatrix(data = pd_test_rf,reference = testing$Class)$overall[1])
 ####glm####
 # set.seed(1990)
 # Fit_glm <- train(Class~.,data=training,method = 'glm')
@@ -170,18 +174,18 @@ print(confusionMatrix(data = pd_test_rf,reference = testing_80$Class)$overall[1]
 #                      classProbs = TRUE,
 #                      summaryFunction = twoClassSummary)
 # 
-# Fit_lda <- train(Class~.,data=training_80,method = 'lda',
+# Fit_lda <- train(Class~.,data=training,method = 'lda',
 #                  trControl = ctrl, metric="ROC")
 # 
 # print(Fit_lda);print(Fit_lda$finalModel)
 # 
-# pd_train_lda <- predict(Fit_lda,training_80)
-# print(confusionMatrix(data = pd_train_lda,reference = training_80$Class)$table)
-# print(confusionMatrix(data = pd_train_lda,reference = training_80$Class)$overall[1])
+# pd_train_lda <- predict(Fit_lda,training)
+# print(confusionMatrix(data = pd_train_lda,reference = training$Class)$table)
+# print(confusionMatrix(data = pd_train_lda,reference = training$Class)$overall[1])
 # 
-# pd_test_lda <- predict(Fit_lda,testing_80)
-# print(confusionMatrix(data = pd_test_lda,reference = testing_80$Class)$table)
-# print(confusionMatrix(data = pd_test_lda,reference = testing_80$Class)$overall[1])
+# pd_test_lda <- predict(Fit_lda,testing)
+# print(confusionMatrix(data = pd_test_lda,reference = testing$Class)$table)
+# print(confusionMatrix(data = pd_test_lda,reference = testing$Class)$overall[1])
 
 
 
